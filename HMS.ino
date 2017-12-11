0b0b63dca61664a435848c19b582d9fd1dc56ca0
Andriy Khusainov <xusik@users.noreply.github.com>
2017-12-11 15:39:17 +0200

    Update HMS.ino

--- HMS.ino
+++ HMS.ino
@@ -96,7 +96,10 @@ void loop(void)
   Serial.println(TempH + "."+ TempL);
   radio.print("T"+TempH + "."+ TempL);
   radio.flush();
-  
+
+  radio.print("sn/r1{\"t\":\""+ TempH + "." + TempL +"\",\"h\":\"" + String(rawhumidity) + "\",\"p\":\"" + Pres +  "\"}");
+  radio.flush();
+    
   sleepSeconds(300);
 }
 
